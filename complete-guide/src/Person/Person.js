1acd6e7eedce4f26164ac9b00504f6e192410e18
Nicky Fahey <nickyrfahey@gmail.com>
2019-08-11 17:51:52 +0100

    Add comments

--- complete-guide/src/Person/Person.js
+++ complete-guide/src/Person/Person.js
@@ -1,6 +1,11 @@
 import React from 'react';
+// use Radium for styles:
 // import Radium from 'radium';
+// use CSS modules for styles
 import styles from './Person.css';
+// more information on using CSS modules:
+// https://facebook.github.io/create-react-app/docs/adding-a-css-modules-stylesheet
+// Note: this project is using an older version of create-react-app
 
 // use function based components as often as possible because they do not have state, and state should be kept to a minimum
 
