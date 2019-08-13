968cc718a739c48cd762e707d4eb9f32781a4596
Nicky Fahey <nickyrfahey@gmail.com>
2019-08-13 21:46:33 +0100

    add propTypes to BurgerIngredient

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from './BurgerIngredient.css'
+import styles from './BurgerIngredient.module.css';
+import PropTypes from 'prop-types';
 
 const burgerIngredient = (props) => {
   let ingredient = null;
@@ -40,5 +41,9 @@ const burgerIngredient = (props) => {
 
   return ingredient;
 }
+
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
 
 export default burgerIngredient;
