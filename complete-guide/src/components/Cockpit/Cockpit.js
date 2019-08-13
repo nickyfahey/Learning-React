6fcba81571ad35caa3bc1578ad4edb02e11e96c2
Nicky Fahey <nickyrfahey@gmail.com>
2019-08-13 17:55:28 +0100

    use useContext and contextType

--- complete-guide/src/components/Cockpit/Cockpit.js
+++ complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import styles from './Cockpit.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
 
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
 
   useEffect(() => {
     toggleBtnRef.current.click();
@@ -76,9 +79,7 @@ const cockpit = (props) => {
         ref={toggleBtnRef}
         className={btnClass}
         onClick={props.clicked}>Toggle Persons</button>
-      <AuthContext.Consumer>
-        {(context) => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
+        <button onClick={authContext.login}>Log in</button>
     </div>
   );
 }
--- complete-guide/src/components/Persons/Person/Person.js
+++ complete-guide/src/components/Persons/Person/Person.js
@@ -29,20 +29,21 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     if (this.props.focus) this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);  
   }
 
   render() {
     return (
       <div className={styles.Person} /*style={style}*/> 
-        <AuthContext.Consumer>
-          {(context) => 
-            context.authenticated 
-              ? <p>Authenticated</p> : <p>Please log in</p>}
-        </AuthContext.Consumer>
-        
+
+        {this.context.authenticated 
+          ? <p>Authenticated</p> : <p>Please log in</p>}
+
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input 
