b25d189bb9ed6a743ba4169aea7ed35565f83076
Nicky Fahey <nickyrfahey@gmail.com>
2019-08-16 21:35:03 +0100

    Add intoerceptors

--- http-ajax/src/index.js
+++ http-ajax/src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from "axios";
+
+// Interceptors
+
+axios.interceptors.request.use(request => {
+  console.log('Request:', request);
+
+  // if the request is not returned the request is blocked
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log('Response:',response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+// To remove an interceptor:
+// var myInterceptor = axios.interceptors.request.use(() => {/*...*/});
+// axios.interceptors.request.eject(myInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
