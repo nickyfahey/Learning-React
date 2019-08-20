7b9b9989b6758f21ff87de2051f82433fe10e820
Nicky Fahey <nickyrfahey@gmail.com>
2019-08-20 17:58:19 +0100

    Add lazy loading examples

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
-import Posts from './Posts/Posts';
-
+import React, { Component, Suspense } from 'react';
 import { Route, NavLink, Switch /*, Redirect */ } from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
+
 import './Blog.css';
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+
+// Only import NewPost when AsyncNewPost is rendered
+// NewPost will have its own bundle which is loaded when needed 
+// and not e included in bundle.js
+
+// Pre react 16.6:
+// import asyncComponent from '../../hoc/asyncComponent';
+// const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
+
+// Lazy Loading React 16.6+:
+const LazyNewPost = React.lazy(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
 
@@ -28,7 +39,17 @@ class Blog extends Component {
 				</header>
 				{/* Use Switch to tell react to render only the first matching route */}
 				<Switch>
-					<Route path="/new-post" component={NewPost} />
+					{/* React < 16.6: */}
+					{/* <Route path="/new-post" component={AsyncNewPost} /> */}
+
+					{/* React >= 16.6: */}
+					<Route path="/new-post" 
+						render={() => {
+							return <Suspense fallback={<div>Loading ..</div>}>
+								<LazyNewPost />
+							</Suspense>
+						}} />
+
 					<Route path="/posts" component={Posts} />
 					{/* catch all route with no path; must be last */}
 					<Route render={() => <h1>Not found</h1>} />
