e76d1b1c89dd0d1e00ee04d05341374b03f8d7d6
Nicky Fahey <nickyrfahey@gmail.com>
2019-09-04 13:54:01 +0100

    Add checkout summary

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder />
+        {/* Only the top level Route component gets access to
+            the routing props; child components will have to be
+            passed needed props, or use the hoc withRouter */}
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/checkout" component={Checkout} />
       </Layout>
     </div>
   );
