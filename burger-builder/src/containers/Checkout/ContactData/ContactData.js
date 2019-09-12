3d513dbd99ad7a3cd8807d342c2229ff5fe096a0
Nicky Fahey <nickyrfahey@gmail.com>
2019-09-12 14:49:31 +0100

    Style contact data form

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import axios from '../../../axios-orders';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 import styles from './ContactData.module.css';
 
@@ -49,10 +50,14 @@ class ContactData extends Component {
   render () {
     let form = (
       <form action="">
-          <input type="text" name="name" placeholder="Your name" />
-          <input type="email" name="email" placeholder="Your email" />
-          <input type="text" name="street" placeholder="Street" />
-          <input type="text" name="postcode" placeholder="Postcode" />
+          <Input inputtype="input" 
+            type="text" name="name" placeholder="Your name" />
+          <Input inputtype="input" 
+            type="email" name="email" placeholder="Your email" />
+          <Input inputtype="input" 
+            type="text" name="street" placeholder="Street" />
+          <Input inputtype="input" 
+            type="text" name="postcode" placeholder="Postcode" />
           <Button btnType="Success" onClick={this.orderHandler}>ORDER</Button>
         </form>
     );
